89738a23111dce4ece52c74cb88449369b0cfd54
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-08-12 13:47:02 +0200

    Created branch-name functionality
    This script makes it possible to infer the current-branch name
    in a git repository.

--- util/navigation/branch-name.pl
+++ util/navigation/branch-name.pl
@@ -2,3 +2,11 @@
 
 use strict;
 use warnings;
+
+my $branches = `git branch`;
+if ($branches =~ m/^\*\s(?<branch>\w+)/m) {
+	my $branch = $+{'branch'};
+	print "$branch\n"
+} else {
+	print "unknown"
+}
