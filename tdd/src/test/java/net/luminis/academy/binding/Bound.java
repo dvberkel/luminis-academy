eef5f955e6e00fdaa3bfc6d29e61ee96f22e1c95
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-08-20 15:57:45 +0200

    UnBound can be bound
    the to method is implemented.

--- tdd/src/test/java/net/luminis/academy/binding/Bound.java
+++ tdd/src/test/java/net/luminis/academy/binding/Bound.java
@@ -26,4 +26,41 @@ public class Bound implements Binding
 	{
 		return template.replaceAll(String.format("\\{\\{%s\\}\\}", variable), this.value);
 	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 37;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		result = prime * result + ((variable == null) ? 0 : variable.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bound other = (Bound) obj;
+		if (value == null)
+		{
+			if (other.value != null)
+				return false;
+		}
+		else if (!value.equals(other.value))
+			return false;
+		if (variable == null)
+		{
+			if (other.variable != null)
+				return false;
+		}
+		else if (!variable.equals(other.variable))
+			return false;
+		return true;
+	}
 }
--- tdd/src/test/java/net/luminis/academy/binding/UnBound.java
+++ tdd/src/test/java/net/luminis/academy/binding/UnBound.java
@@ -3,16 +3,19 @@ package net.luminis.academy.binding;
 public class UnBound implements Binding
 {
 
+	private final String variable;
+
 	public UnBound(String variable)
 	{
 		if (variable == null)
 			throw new IllegalArgumentException();
+		this.variable = variable;
 	}
 
 	@Override
-	public Binding to(String string)
+	public Binding to(String value)
 	{
-		return null;
+		return new Bound(variable, value);
 	}
 
 	@Override
