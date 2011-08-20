c15d3f524068b7a1e7d39a7315b32fe9472dd6f5
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-08-20 14:47:35 +0200

    Added binding tests.
    This is an example where one step is for free when later on a
    change has to be made.

--- tdd/src/test/java/net/luminis/academy/BindingTest.java
+++ tdd/src/test/java/net/luminis/academy/BindingTest.java
@@ -5,11 +5,27 @@ import net.luminis.academy.binding.Bound;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+
 public class BindingTest
 {
 	@Test(expected = IllegalArgumentException.class)
 	public void shouldFailWithoutAVariable()
 	{
 		Binding binding = new Bound(null, "values");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void boundShouldFailWithoutAVariable()
+	{
+		Binding binding = new Bound("variable", null);
+	}
+
+	@Test
+	public void boundShouldBeCreatedWithAVariableAndAValue()
+	{
+		Binding binding = new Bound("variable", "value");
+
+		assertNotNull(binding);
 	}
 }
