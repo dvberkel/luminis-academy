76841ada3ca46e047848a08dee05899a712e3258
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-08-20 15:46:12 +0200

    Tests passes

---
+++ tdd/src/test/java/net/luminis/academy/binding/UnBound.java
@@ -0,0 +1,24 @@
+package net.luminis.academy.binding;
+
+public class UnBound implements Binding
+{
+
+	public UnBound(String variable)
+	{
+		if (variable == null)
+			throw new IllegalArgumentException();
+	}
+
+	@Override
+	public Binding to(String string)
+	{
+		return null;
+	}
+
+	@Override
+	public String apply(String template)
+	{
+		return null;
+	}
+
+}
