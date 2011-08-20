d169e787ea71a9b7688698c6f4ab9fbf2ffaced1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-08-20 08:43:37 +0200

    Added first test
    The first test is added

---
+++ tdd/src/test/java/net/luminis/academy/TemplateTest.java
@@ -0,0 +1,14 @@
+package net.luminis.academy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class TemplateTest
+{
+	@Test
+	public void shouldExist()
+	{
+		assertNotNull(TemplatePress.class);
+	}
+}
