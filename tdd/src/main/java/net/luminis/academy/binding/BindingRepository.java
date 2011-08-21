1cd039e72aebfa98ddfa4d3eef5dda818dbf2835
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-08-21 13:11:45 +0200

    BindRepository passes test
    The binding repository now knows what to do about bindings.

--- tdd/src/main/java/net/luminis/academy/binding/BindingRepository.java
+++ tdd/src/main/java/net/luminis/academy/binding/BindingRepository.java
@@ -1,9 +1,16 @@
 package net.luminis.academy.binding;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class BindingRepository
 {
 
 	private final String template;
+
+	private final Map<String, Binding> bindings = new HashMap<String, Binding>();
 
 	public BindingRepository(String template)
 	{
@@ -11,6 +18,19 @@ public class BindingRepository
 			throw new IllegalArgumentException();
 
 		this.template = template;
+		initialize();
+	}
+
+	private void initialize()
+	{
+		Pattern pattern = Pattern.compile("\\{\\{([^}]+)\\}\\}");
+		Matcher matcher = pattern.matcher(template);
+		while (matcher.find())
+		{
+			String variable = matcher.group(1);
+			bindings.put(variable, new BindingWrapper(new UnBound(variable)));
+		}
+
 	}
 
 	public Binding bind(String variable)
@@ -18,6 +38,41 @@ public class BindingRepository
 		if (!template.contains(String.format("{{%s}}", variable)))
 			throw new IllegalStateException();
 
-		return new UnBound(variable);
+		return bindings.get(variable);
+	}
+
+	public String apply()
+	{
+		String result = template;
+		for (Binding binding : bindings.values())
+		{
+			result = binding.apply(result);
+		}
+		return result;
+	}
+}
+
+class BindingWrapper implements Binding
+{
+	private Binding binding;
+
+	public BindingWrapper(Binding binding)
+	{
+		if (binding == null)
+			throw new IllegalArgumentException();
+		this.binding = binding;
+	}
+
+	@Override
+	public Binding to(String value)
+	{
+		this.binding = this.binding.to(value);
+		return this.binding;
+	}
+
+	@Override
+	public String apply(String template)
+	{
+		return this.binding.apply(template);
 	}
 }
