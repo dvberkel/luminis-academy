7f8e66fae2706f89a9781217885f8eba07efb024
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-08-21 12:44:06 +0200

    Trivial implementation for binding
    It just returns a new Unbound variable.

--- tdd/src/main/java/net/luminis/academy/binding/BindingRepository.java
+++ tdd/src/main/java/net/luminis/academy/binding/BindingRepository.java
@@ -3,14 +3,21 @@ package net.luminis.academy.binding;
 public class BindingRepository
 {
 
+	private final String template;
+
 	public BindingRepository(String template)
 	{
 		if (template == null)
 			throw new IllegalArgumentException();
+
+		this.template = template;
 	}
 
 	public Binding bind(String variable)
 	{
-		throw new IllegalStateException();
+		if (!template.contains(String.format("{{%s}}", variable)))
+			throw new IllegalStateException();
+
+		return new UnBound(variable);
 	}
 }
