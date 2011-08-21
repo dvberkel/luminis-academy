9d8c5593e36cc47c5df71e0791b0832b0c707fa1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-08-21 10:15:54 +0200

    BindingRepository with template
    A BindingRepository should be instantiated with a template.

--- tdd/src/test/java/net/luminis/academy/binding/BindingRepositoryTest.java
+++ tdd/src/test/java/net/luminis/academy/binding/BindingRepositoryTest.java
@@ -2,11 +2,19 @@ package net.luminis.academy.binding;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+
 public class BindingRepositoryTest
 {
 	@Test(expected = IllegalArgumentException.class)
 	public void shouldFailWhenInstantiatedWithoutATemplate()
 	{
 		new BindingRepository(null);
+	}
+
+	@Test
+	public void shouldBeCreatedWithATemplate()
+	{
+		assertNotNull(new BindingRepository("Hello World"));
 	}
 }
