c1244243fc85f17bdc529d5493d9b13855478795
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-08-21 10:03:19 +0200

    Unbound is value object
    The unbound class is a value object.

--- tdd/src/main/java/net/luminis/academy/binding/UnBound.java
+++ tdd/src/main/java/net/luminis/academy/binding/UnBound.java
@@ -24,4 +24,39 @@ public class UnBound implements Binding
 		throw new IllegalStateException();
 	}
 
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((variable == null) ? 0 : variable.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UnBound other = (UnBound) obj;
+		if (variable == null)
+		{
+			if (other.variable != null)
+				return false;
+		}
+		else if (!variable.equals(other.variable))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString()
+	{
+		return String.format("{{%s}}", variable);
+	}
+
 }
